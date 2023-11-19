28466a4837df4fdc88d2ba730f409a4a3cef6a1d
jonfre8512 <51750163+jonfre8512@users.noreply.github.com>
2023-11-19 07:45:37 -0500

    Add files via upload

---
+++ java scrip/anima.js
@@ -0,0 +1,9 @@
+  function mostrarInicioSesion() {
+    document.getElementById("registroForm").style.display = "none";
+    document.getElementById("inicioSesionForm").style.display = "block";
+  }
+  
+  function mostrarRegistro() {
+    document.getElementById("inicioSesioinFrom").style.display = "none";
+    document.getElementById("registroFrom").style.display = "block";
+  }
